71cf9f95b8da2ee013a10d05dfca24d2dc96aa98
Matthew Ho <homatt999@gmail.com>
2022-10-24 10:38:40 -0700

    for create cluster

--- createcluster.sh
+++ createcluster.sh
@@ -1,5 +1,6 @@
-# Script for creating helix clusters on shared or gobblin corp-ltx1 helix clusters
+# Script for creating helix clusters on shared or gobblin ${zk_env}-ltx1 helix clusters
 # "shared" or "gobblin"
+zk_env = "corp"
 zk_cluster="gobblin"
 clusters_to_create=(
   # e.g. "gobblin-kafka-streaming-tracking-test-ltx1-holdem-test-1"
@@ -23,11 +24,11 @@ for cluster_name in ${clusters_to_create[@]}
 do
   echo "${cluster_name}"
   echo "1. Create ${cluster_name}"
-  curl -X PUT "http://hr-ltx1.corp.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}"
+  curl -X PUT "http://hr-ltx1.${zk_env}.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}"
   sleep 5
   json=" { \"id\" : \"${cluster_name}\", \"simpleFields\" : { \"allowParticipantAutoJoin\" : \"true\"} } "
-  curl -X POST -H "Content-Type: application/json" "hr-ltx1.corp.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}/configs?command=update" -d "${json}"
+  curl -X POST -H "Content-Type: application/json" "hr-ltx1.${zk_env}.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}/configs?command=update" -d "${json}"
   echo "2. Register ${cluster_name}"
-  curl -X POST "http://hr-ltx1.corp.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}?command=activate&superCluster=${super_cluster}"
-  curl "hr-ltx1.corp.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}/configs"
+  curl -X POST "http://hr-ltx1.${zk_env}.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}?command=activate&superCluster=${super_cluster}"
+  curl "hr-ltx1.${zk_env}.linkedin.com:12954/admin/v2/namespaces/${zk_cluster}/clusters/${cluster_name}/configs"
 done
